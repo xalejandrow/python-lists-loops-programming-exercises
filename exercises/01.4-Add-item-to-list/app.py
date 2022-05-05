8743b3626cedde0dc7cb7c462c9105ccb2f663a2
Alew <xalejandrow@gmail.com>
2022-05-05 18:34:28 -0300

    ej01.4

--- exercises/01.4-Add-item-to-list/app.py
+++ exercises/01.4-Add-item-to-list/app.py
@@ -1,5 +1,11 @@
 #Remember import random function here:
+import random
 
 my_list = [4,5,734,43,45]
 
 #The magic is here:
+for i in range(0,10):
+    num = random.randint(0,10)
+    my_list.append(num)
+
+print(my_list)
