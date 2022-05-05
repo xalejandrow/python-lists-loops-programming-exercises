ef979b72519ee4d0488e8fabf058e60daddb439e
Alew <xalejandrow@gmail.com>
2022-05-05 19:37:50 -0300

    ej02.1

--- exercises/02.1-Loop-from-the-top/app.py
+++ exercises/02.1-Loop-from-the-top/app.py
@@ -7,3 +7,10 @@ my_sample_list = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12]
 # while some_variable_reference > 0:
 #     print(my_sample_list[some_variable_reference])
 #     chage after doing what we wanted, change the variables value so that the next pass it references the new index position
+some_variable_reference = len(my_sample_list) -1
+while some_variable_reference >= 0:
+    print(my_sample_list[some_variable_reference])
+    some_variable_reference -= 1
+print('\n')
+for i in range(len(my_sample_list) - 1, -1, -1):
+    print(my_sample_list[i])
