dc11c28da8efc411a24dd2834d54209ada50bf4f
Alew <xalejandrow@gmail.com>
2022-05-06 20:23:23 +0000

    ej13

--- .learn/resets/13-Filter_list/app.py
+++ .learn/resets/13-Filter_list/app.py
@@ -2,8 +2,6 @@ allNumbers = [23,12,35,5,3,2,3,54,3,21,534,23,42,1]
 
 
 def filter_function(items):
-    if items > 10:
-        return items
-    #return items % 2 ==0
+    return items % 2 ==0
 greater_than = list(filter(filter_function, allNumbers))
 print(greater_than)
