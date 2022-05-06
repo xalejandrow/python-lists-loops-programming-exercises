34f0565cdb2f2e3a905f562a82e59d6aaab53985
Alew <xalejandrow@gmail.com>
2022-05-06 10:10:42 -0300

    ej06.1

--- exercises/06.1-Everything_is_awesome/app.py
+++ exercises/06.1-Everything_is_awesome/app.py
@@ -4,6 +4,10 @@ def my_function(numbers):
     new_list = []
     for numb in numbers:
     #The magic go here:
+        if numb == 1:
+            new_list.append(numb)
+        else:
+            new_list.append("Yahoo")
         
     return new_list
 print(my_function(my_list))
