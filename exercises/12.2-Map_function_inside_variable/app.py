3d55ff6c63402f82bb7893668c412d0acd2ebbc7
Alew <xalejandrow@gmail.com>
2022-05-06 11:26:42 -0300

    ej12.2

--- exercises/12.2-Map_function_inside_variable/app.py
+++ exercises/12.2-Map_function_inside_variable/app.py
@@ -3,3 +3,5 @@ names = ['Alice','Bob','Marry','Joe','Hilary','Stevia','Dylan']
 def prepender(name):
     return "My name is: " + name
 #Your code go here:
+new_list = list(map(prepender, names))
+print(new_list)
