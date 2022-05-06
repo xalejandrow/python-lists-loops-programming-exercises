f91b6309d1662ab74af868ff21801b44ba6e52d1
Alew <xalejandrow@gmail.com>
2022-05-06 20:31:07 +0000

    ej13

--- .learn/resets/13.1-Filter_and_list/app.py
+++ .learn/resets/13.1-Filter_and_list/app.py
@@ -2,11 +2,7 @@
 all_names = ["Romario","Boby","Roosevelt","Emiliy", "Michael", "Greta", "Patricia", "Danzalee"]
 
 #Your code go here:
-def my_function(name):
-    if name[0] == 'R':
-        return name
 
-resulting_names = list(filter(my_function, all_names))
 print(resulting_names)
 
 
