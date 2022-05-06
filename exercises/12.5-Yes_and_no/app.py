50bb7c26bdb5a6f46cc2083991e148bc76dd7c90
Alew <xalejandrow@gmail.com>
2022-05-06 11:57:54 -0300

    ej12.5

--- exercises/12.5-Yes_and_no/app.py
+++ exercises/12.5-Yes_and_no/app.py
@@ -1,5 +1,12 @@
 theBools = [0,1,0,0,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,1]
 
 #Your code go here:
+def changeToText(num):
+    if num == 1:
+        return 'wiki'
+    else:
+        return 'woko'
 
+new_list = list(map(changeToText,theBools))
+print(new_list)
 
