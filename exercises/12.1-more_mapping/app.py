babf935d1ae7bb935730ee02b6738800a2686e6b
Alew <xalejandrow@gmail.com>
2022-05-06 11:24:29 -0300

    ej12.1

--- exercises/12.1-more_mapping/app.py
+++ exercises/12.1-more_mapping/app.py
@@ -1,5 +1,9 @@
 myNumbers = [23,234,345,4356234,243,43,56,2]
 
 #Your code go here:
+def increment_by_one(num):
+    return num * 3
+
+new_list = list(map(increment_by_one, myNumbers))
 
 print(new_list)
