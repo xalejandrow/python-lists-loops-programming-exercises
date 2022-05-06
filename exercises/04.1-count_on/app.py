b03309ee107019e81d84161170502d0d84da9e4c
Alew <xalejandrow@gmail.com>
2022-05-05 21:00:14 -0300

    ej04.1

--- exercises/04.1-count_on/app.py
+++ exercises/04.1-count_on/app.py
@@ -1,3 +1,9 @@
 my_list = [42, True, "towel", [2,1], 'hello', 34.4, {"name": "juan"}]
 
 #your code go here:
+new_list = []
+for i in my_list:
+    if str(type(i)) == "<class 'dict'>" or str(type(i)) == "<class 'list'>":
+        new_list.append(i)
+
+print(new_list)
