7a9cdaa993343047be734fc68c03f686fb9a520a
Alew <xalejandrow@gmail.com>
2022-05-06 09:51:40 -0300

    ej01.1

--- exercises/01.1-Access-and-Retrieve/app.py
+++ exercises/01.1-Access-and-Retrieve/app.py
@@ -2,7 +2,7 @@
 my_list = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
 
 # 1. print the item here
-print(2)
+print(my_list[2])
 # 2. change the position were 'thursday' is to None
 my_list[4] = None
 # 3. print that position now here
