096a1a8a3d8c6cca32b25bec80956841b83479a3
Alew <xalejandrow@gmail.com>
2022-05-06 10:33:02 -0300

    ej08

--- exercises/08-Delete_element/app.py
+++ exercises/08-Delete_element/app.py
@@ -3,6 +3,11 @@ people = ['juan','ana','michelle','daniella','stefany','lucy','barak']
 #Your code go here:
 def deletePerson(person_name):
     #Your code go here:
+    new_list = []
+    for person in people:
+        if person != person_name:
+            new_list.append(person)
+    return new_list
     
 print(deletePerson("daniella"))
 print(deletePerson("juan"))
