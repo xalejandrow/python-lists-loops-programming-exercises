21366c6ea385355072f9b29c7ae070eaf1722247
Alew <xalejandrow@gmail.com>
2022-05-06 09:39:31 -0300

    ej05.1

--- exercises/05.1-sum_odd_items/app.py
+++ exercises/05.1-sum_odd_items/app.py
@@ -1,5 +1,15 @@
 arr = [4,5,734,43,45,100,4,56,23,67,23,58,45]
 
 #Your code go here:
+def sum_odd_values(items):
+
+    total= 0
+    #The magic happens here:
+    for i in arr:
+        if i % 2 != 0:
+            total += i
+    return total
+
+print(sum_odd_values(arr))
 
 
