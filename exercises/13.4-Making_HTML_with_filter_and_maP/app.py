130d65535bb5ef25b1a398c5f4fb3af6f4030440
Alew <xalejandrow@gmail.com>
2022-05-12 22:08:02 +0000

    ej13.4

--- exercises/13.4-Making_HTML_with_filter_and_maP/app.py
+++ exercises/13.4-Making_HTML_with_filter_and_maP/app.py
@@ -9,4 +9,19 @@ all_colors = [
 ]
 
 #Your code go here:
+def generate_li(item):
+	#if item != None:
+		#return item
+	return '<li>'+item+'</li>'
 
+def filter_colors(color):
+	if color['sexy'] == True:
+		return color
+
+#new_colors = list(filter(generate_li,all_colors))
+#new_colors = list(filter(filter_colors, map( lambda color: '<li>' + color['label'] + '</li>',all_colors)))
+new_colors = list(filter(filter_colors, all_colors))
+new_colors_map = list(map( lambda color: generate_li(color['label']),new_colors))
+#new_colors_map = map( lambda color: generate_li(color['label']),new_colors)
+#print(new_colors)
+print(new_colors_map)
