0c4a29d5caf79a151fa8082c185fe49beb4a9b74
Alew <xalejandrow@gmail.com>
2022-05-14 22:31:02 +0000

    ej16

--- exercises/16-Techno_beat/app.py
+++ exercises/16-Techno_beat/app.py
@@ -1,4 +1,19 @@
-
+def lyrics_generator(matrix):
+    result = ""
+    text1 = "Boom "
+    text2 = "Drop the base "
+    text3 = "!!!Break the base!!! "
+    aux = 0
+    for num in matrix:
+        if num == 0:
+            result += text1
+        if num == 1:
+            result += text2
+            aux += 1
+        if aux == 3:
+            result += text3
+            aux = 0
+    return result
 
 # Your code go above, nothing to change after this line:
 print(lyrics_generator([0,0,1,1,0,0,0]))
