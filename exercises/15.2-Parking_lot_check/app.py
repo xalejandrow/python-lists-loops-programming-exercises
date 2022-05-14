0fddc243a72cd486868e2ddfca3c4cd4f11fd843
Alew <xalejandrow@gmail.com>
2022-05-14 22:19:57 +0000

    ej15.2

--- exercises/15.2-Parking_lot_check/app.py
+++ exercises/15.2-Parking_lot_check/app.py
@@ -5,3 +5,22 @@ parking_state = [
 ]
 
 #Your code go here:
+
+
+def get_parking_lot(parking):
+  state = {
+     "total_slots": 0,
+     "available_slots": 0,
+     "occupied_slots": 0
+  }
+  for row in parking: 
+    for col in row:
+      if col != 0:
+        state["total_slots"] += 1
+      if col == 2:
+        state["available_slots"] += 1
+      if col == 1:
+        state["occupied_slots"] += 1
+  return state
+state = get_parking_lot(parking_state)
+print(state)
