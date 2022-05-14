acff2950db0a1ec26a9ceb6c76501e1985685120
Alew <xalejandrow@gmail.com>
2022-05-14 21:15:12 +0000

    ej14

--- exercises/14-Loop-dictionary/app.py
+++ exercises/14-Loop-dictionary/app.py
@@ -1,8 +1,13 @@
 spanish_translations = { "dog": "perro", "house": "casa", "cat": "gato" }
 # Your code here
-
+spanish_translations["love"] = "amor"
+spanish_translations["code"] = "codigo"
+spanish_translations["smart"] = "inteligente"
 
 # Don't touch the code below
 print("Translation", spanish_translations["dog"])
 print(spanish_translations)
 
+#for key in spanish_translations:
+#    print(key) # <- output: "dog",  "house", "cat"
+#    print(spanish_translations[key]) # <- output: "perro",  "casa", "gato"
