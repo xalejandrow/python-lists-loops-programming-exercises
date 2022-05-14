2d49bc0a37ed54a6972ca9676f136dfaf2edd6b5
Alew <xalejandrow@gmail.com>
2022-05-14 21:42:40 +0000

    ej14.2

--- exercises/15.1-Matrix_Builder/app.py
+++ exercises/15.1-Matrix_Builder/app.py
@@ -1,3 +1,14 @@
 #Import random
-
+import random
 #Create the function below:
+def matrixBuilder(num):
+    matrix = []
+    for i in range(0,num):
+        matAux = []
+        for j in range(0,num):
+            matAux.append(1)
+        matrix.append(matAux)
+    return matrix
+
+print(matrixBuilder(3))
+
